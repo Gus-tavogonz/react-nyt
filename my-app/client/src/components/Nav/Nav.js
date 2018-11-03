35380183e09ad96ceb32cbfb2a5d0acb76c914be
unknown <gustavogonz3393@gmail.com>
2018-11-03 01:56:00 -0500

    initial test

---
+++ my-app/client/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark">
+    <a className="navbar-brand" href="/">
+    New York Times - Search for any Article!
+    </a>
+  </nav>
+);
+
+export default Nav;
